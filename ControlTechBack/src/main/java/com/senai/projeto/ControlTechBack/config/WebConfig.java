e83966471b8339d243412a0a80c5fef792d5213f
V1ct0rhugo22 <victorhero53@gmail.com>
2025-12-03 21:28:04 -0300

    correções

--- ControlTechBack/src/main/java/com/senai/projeto/ControlTechBack/config/WebConfig.java
+++ ControlTechBack/src/main/java/com/senai/projeto/ControlTechBack/config/WebConfig.java
@@ -8,19 +8,21 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc // Certifique-se de que esta anotação está presente para habilitar o WebMvcConfigurer
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Aplica a TODOS os endpoints da sua API
+        registry.addMapping("/**")
                 .allowedOrigins(
                         // ORIGENS PERMITIDAS
                         "http://localhost:3000",
                         "http://localhost:8080",
-                        "https://control-tech-six.vercel.app" // Esta é a origem correta.
+                        "https://control-tech-six.vercel.app",
+                        "https://controltech.up.railway.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*"); // Permite todos os cabeçalhos
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
